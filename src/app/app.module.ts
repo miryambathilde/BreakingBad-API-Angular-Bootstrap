0ec860622d8f555482486dd618d2d304992ddbd1
Miryam <mbathilde@gmail.com>
2021-08-25 13:21:46 +0200

    httpClient and get All in services Character

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPersonajesComponent } from './components/lista-personajes/lista-personajes.component';
@@ -14,7 +15,8 @@ import { PersonajeComponent } from './components/personaje/personaje.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import { Character } from "../interfaces/character.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
+  baseUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'https://www.breakingbadapi.com/api/';
+  }
+
+  getAll() : Promise<Character[]> {
+    // es una PROMESA y se resuelve con: then-catch o con async-await
+    return this.httpClient.get<Character[]>(this.baseUrl).toPromise();
+  }
 }
