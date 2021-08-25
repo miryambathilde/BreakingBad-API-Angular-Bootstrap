b924227ee0b50c3cec75ad8ea5025fe5543dac46
Miryam <mbathilde@gmail.com>
2021-08-25 13:12:08 +0200

    interface and service character created

---
+++ src/app/interfaces/character.interface.ts
@@ -0,0 +1,12 @@
+export interface Character {
+  id: number;
+  name: string;
+  birthday: string;
+  occupation: [];
+  img: string;
+  status: string;
+  nickname: string;
+  appearance: [];
+  portrayed: string;
+  category: [];
+}
